4ba09fc266ffed09996b285f373967c43b21576d
Jay <jay.nguyen@students.fhu.edu>
2024-02-15 12:12:13 -0600

    Update main.py
    removed comments and corrected the argument

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ class Queue():
     def __init__(self):
         self.cards = []
 
-    #REMOVE PASS AND COMPLETE THE FUNCTION
+
     def push(self, card):
         self.cards.append(card)
 
-    #REMOVE PASS AND COMPLETE THE FUNCTION
+
     def pop(self):
-        del self.block[-1]
+        del self.cards[-1]
 
 if __name__ == '__main__':
     #REMOVE PASS AND YOUR CODE GOES HERE
